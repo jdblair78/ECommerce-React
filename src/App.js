379868103cc1c3f28ca224c66b3509ae9cf43441
jdblair78 <jdblair78@gmail.com>
2026-02-08 18:16:10 -0500

    Ecommerce

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on port", PORT));
+
+
+
+
+
+
+
+
+
 import Landing from "./components/Landing";
 import Nav from "./components/Nav";
 import { BrowserRouter as Router, Route } from "react-router-dom";
