7234671129bcc2a5e108c968fa5d2b13c012c616
jdblair78 <jdblair78@gmail.com>
2026-02-08 18:25:14 -0500

    Ecommerce

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,55 +1,53 @@
-import { icon } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
 import Price from "./Price";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 const Book = ({ book }) => {
-  const [img, setImg] = useState();
-
-  const mountedRef = useRef(true);
+  const [img, setImg] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const image = new Image();
     image.src = book.url;
+
     image.onload = () => {
       setTimeout(() => {
-        if (mountedRef.current) {
-          setImg(image);
-        }
+        setImg(image);
+        setIsLoading(false);
       }, 300);
     };
-    return () => {
-      mountedRef.current = false;
-    };
-  });
+  }, [book.url]);
 
   return (
     <div className="book">
-      {img ? (
-        <>
-          <Link to={`/books/${book.id}`}>
-            <figure className="book_img--wrapper"></figure>
-            <img src={img.src} alt="" className="book__img" />
-          </Link>
-          <div className="book__title">
-            <Link to={`/books/${book.id}`} className="book__title--link">
-              {book.title}
-            </Link>
-          </div>
-          <Rating rating={book.rating} />
-          <Price
-            originalPrice={book.originalPrice}
-            salePrice={book.salePrice}
-          />
-        </>
-      ) : (
+      {isLoading ? (
         <>
           <div className="book__img--skeleton"></div>
           <div className="skeleton book__title--skeleton"></div>
           <div className="skeleton book__rating--skeleton"></div>
           <div className="skeleton book__price--skeleton"></div>
+        </>
+      ) : (
+        <>
+          <Link to={`/books/${book.id}`}>
+            <figure className="book_img--wrapper"></figure>
+            <img src={img.src} alt="" className="book__img" />
+          </Link>
+
+          <div className="book__title">
+            <Link to={`/books/${book.id}`} className="book__title--link">
+              {book.title}
+            </Link>
+          </div>
+
+          <Rating rating={book.rating} />
+
+          <Price
+            originalPrice={book.originalPrice}
+            salePrice={book.salePrice}
+          />
         </>
       )}
     </div>
